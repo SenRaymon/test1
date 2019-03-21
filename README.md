2520beb57defdbcbfa9f3626b509eed74c5aa5b1
raymon <admin@test.com>
2019-03-21 18:08:35 +0800

    log

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# test1
-the new state of learning git
+## MyHome
+
+
+
---
+++ src/com/test/Main.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i = 5;
+		int j = 4;
+		System.out.println(j);
+		System.out.println(i);
+		System.out.println("ok");
+
+
+		
+	}
+
+}
